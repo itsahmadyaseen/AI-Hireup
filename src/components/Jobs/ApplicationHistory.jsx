c8dcc436d07ebe8eacccc6693ed67d565cc6eb86
Abdul Rahman <abdulrahman7212@gmail.com>
2025-08-13 15:32:12 +0530

    Updated ApplicationHistory page

--- src/components/Jobs/ApplicationHistory.jsx
+++ src/components/Jobs/ApplicationHistory.jsx
@@ -147,13 +147,16 @@ export default function ApplicationHistory() {
                         View Job
                       </button>
                       <button
-                        onClick={() => router.push(`/jobs/${application.jobId}`)}
-                        className="flex items-center text-blue-600 hover:text-blue-500 text-sm font-medium border rounded-md p-2 cursor-pointer"
-                      >
+                        onClick={() => {
+                        window.open(
+                            "http://localhost:3001/#cam=1&mic=1&screen=0&video=1&audio=1&chat=1&theme_color=cyan",
+                              "_blank");
+                          }}
+                            className="px-6 py-3 bg-blue-600 text-white rounded hover:bg-blue-700">
+                              
                         Start Interview
                       </button>
                     </div>
-                    
                   </div>
                 </div>
               </div>
